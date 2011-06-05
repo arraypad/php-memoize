29d7f4544adc746aeb89c0cb7277d937d9a79ae2
Arpad Ray <arraypad@gmail.com>
2011-06-05 20:58:45 +0100

    fix indentation

--- memoize.c
+++ memoize.c
@@ -1,18 +1,18 @@
 /*
   +----------------------------------------------------------------------+
-  | PHP Version 5                                                        |
+  | PHP Version 5														|
   +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2011 The PHP Group                                |
+  | Copyright (c) 1997-2011 The PHP Group								|
   +----------------------------------------------------------------------+
-  | This source file is subject to version 3.01 of the PHP license,      |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_01.txt                                  |
+  | This source file is subject to version 3.01 of the PHP license,	  |
+  | that is bundled with this package in the file LICENSE, and is		|
+  | available through the world-wide-web at the following url:		   |
+  | http://www.php.net/license/3_01.txt								  |
   | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
+  | obtain it through the world-wide-web, please send a note to		  |
+  | license@php.net so we can mail you a copy immediately.			   |
   +----------------------------------------------------------------------+
-  | Author: Arpad Ray <arpad@php.net>                                    |
+  | Author: Arpad Ray <arpad@php.net>									|
   +----------------------------------------------------------------------+
 */
 
@@ -211,8 +211,8 @@ PHP_FUNCTION(memoize_call)
 		}
 		RETURN_FALSE;
 	}
-    ctxt.copy = APC_COPY_OUT_USER;
-    ctxt.force_update = 0;
+	ctxt.copy = APC_COPY_OUT_USER;
+	ctxt.force_update = 0;
 
 	/* look up key in apc */
 	t = apc_time();
